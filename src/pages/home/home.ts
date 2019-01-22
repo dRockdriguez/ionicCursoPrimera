b26edc9cede7676d11c4d7490a3401eb9f38e49c
davidrodriguezonetec <david.rodriguez@grupoonetec.com>
2019-01-22 09:48:02 +0100

    terminado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ANIMALES } from "../../data/data.animales";
+import { Animal } from '../../interfaces/animal.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales: Animal [] = []
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando: boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
+    this.animales = ANIMALES.slice(0);
+  }
 
+  play(animal: Animal) {
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo){
+      animal.reproduciendo = false;
+    } else {
+      this.audio.src = animal.audio;
+      animal.reproduciendo = true;
+
+      this.audio.load()
+      this.audio.play();
+
+      this.audioTiempo = setTimeout(() => {
+        animal.reproduciendo = false;
+      }, animal.duracion * 1000);
+    }
+  }
+
+  private pausar_audio(animal: Animal){
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let a of this.animales) {
+      if (a.nombre != animal.nombre){
+        a.reproduciendo = false;
+      }
+    }
+  }
+
+  borrar_animal (i: number){
+    this.animales.splice(i, 1);
+  }
+
+  recargar (e: Refresher){
+    setTimeout(() => {
+      this.animales = ANIMALES.slice(0);
+      e.complete();
+    }, 800);
+  }
+
+  reordenar (e) {
+    this.animales = reorderArray(this.animales, e);
   }
 
 }
